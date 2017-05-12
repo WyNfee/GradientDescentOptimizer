e67a2fc1e904d7877002f70f5b611b73e8ecf940
WyNfee <wangyifei52199@sina.com>
2017-05-12 13:42:11 +0800

    add momentum optimizer

--- script_optimizer_comparison.m
+++ script_optimizer_comparison.m
@@ -3,12 +3,14 @@ clear; close all; clc;
 
 
 %load the cost data
-load('data_lost_momentum.mat');
 load('data_lost_no_optimizer.mat');
+load('data_lost_momentum.mat');
+load('data_lost_nag.mat');
 
 %plot them out
 plot(t_cost_data_no_optimizer(:,1), t_cost_data_no_optimizer(:, 2), 'g-',...
-        t_cost_data_momentum(:,1), t_cost_data_momentum(:,2),'r-'...
+        t_cost_data_momentum(:,1), t_cost_data_momentum(:,2),'r-',...
+        t_cost_data_nag(:,1), t_cost_data_nag(:,2),'b-'...
         ...
         );
 legend('No Optimizer', 'Momentum');
